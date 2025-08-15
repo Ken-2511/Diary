5f37553e7a6024cfcab616bab45bd24199152510
Ken <yongkang.cheng@mail.utoronto.ca>
2025-08-15 12:04:40 -0400

    test

--- diary.py
+++ diary.py
@@ -1,17 +1,22 @@
-import os
-import tomllib
-from datetime import datetime
+try:
+    import os
+    import tomllib
+    from datetime import datetime
 
-# load config
-with open('config/config.toml', 'rb') as f:
-    config = tomllib.load(f)
+    # load config
+    with open('config/config.toml', 'rb') as f:
+        config = tomllib.load(f)
 
-# create the diary file
-now = datetime.now()
-dir_name = os.path.join(config['diary_dir'], now.strftime('%Y-%m-%d-%H-%M-%S'))
-os.mkdir(dir_name)
-with open(os.path.join(dir_name, config['diary_name']), 'w') as f:
-    pass
+    # create the diary file
+    now = datetime.now()
+    dir_name = os.path.join(config['diary_dir'], now.strftime('%Y-%m-%d-%H-%M-%S'))
+    os.mkdir(dir_name)
+    with open(os.path.join(dir_name, config['diary_name']), 'w') as f:
+        pass
 
-# open text app
-os.system(f"{config['text_app']} \"{os.path.join(dir_name, config['diary_name'])}\"")
+    # open text app
+    os.system(f"{config['text_app']} \"{os.path.join(dir_name, config['diary_name'])}\"")
+
+except Exception as e:
+    print(e)
+    input('Press Enter to continue...')
