7239fd4e404685bf0b78b26b53f960c8b4faf7e6
Ken <yongkang.cheng@mail.utoronto.ca>
2026-01-18 12:02:55 -0500

    it seems like there was a bug of embedding the folder name instead of embedding the content... so silly

--- utils/filt_dir.py
+++ utils/filt_dir.py
@@ -22,8 +22,10 @@ def get_embedding(dir_name: str) -> np.ndarray:
     if os.path.exists(path):
         return np.load(path)
     else:
+        with open(os.path.join(config['diary_dir'], dir_name, config['diary_name']), 'r', encoding='utf-8') as f:
+            content = f.read()
         response = client.embeddings.create(
-            input=dir_name,
+            input=content,
             model="text-embedding-3-large"
         )
         vec = response.data[0].embedding
@@ -34,22 +36,21 @@ def get_embedding(dir_name: str) -> np.ndarray:
 
 def filt_dir(dir_names: list[str], target_dir_name: str, top_n: int) -> list[str]:
     dirs = [{'name': dn} for dn in dir_names]
-    # embedding
+    # similarity score
     target_dir_vec = get_embedding(target_dir_name)
     for dir in dirs:
         dir['vec'] = get_embedding(dir['name'])
         dir['similarity'] = get_similarity(dir['vec'], target_dir_vec)
-    # time distance
     target_dir_time = datetime.strptime(target_dir_name, '%Y-%m-%d-%H-%M-%S')
+    # recency score
     for dir in dirs:
         dir['time'] = datetime.strptime(dir['name'], '%Y-%m-%d-%H-%M-%S')
         dir['time_distance'] = abs(dir['time'] - target_dir_time)
         dir['time_distance_days'] = dir['time_distance'] / timedelta(days=1)
-    # score
-    for dir in dirs:
-        dir['vec_score'] = 15 * dir['similarity']
-        dir['time_score'] = -1 * math.log(dir['time_distance_days'] + 1)
-        dir['total_score'] = dir['vec_score'] + dir['time_score']
+            # Time decay using half-life: freshness = exp(-ln(2) * days_ago / half_life)
+        dir['recency_score'] = math.exp(-0.693 * dir['time_distance_days'] / config['half_life_days'])
+            # total score = similarity score * recency score
+        dir['total_score'] = config['vec_score_weight'] * dir['similarity'] + config['recency_weight'] * dir['recency_score']
     # sort
     dirs.sort(key=lambda x: x['total_score'], reverse=True)
     return [dir['name'] for dir in dirs[:top_n]]
